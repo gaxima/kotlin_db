ad422aabc2931e3619e14fdc7f03d1ee832e9536
gabriel-ximenes-telus <gabriel.ximenes@telus.com>
2024-07-24 13:37:20 -0300

    remove database connection

--- src/main/kotlin/br/com/alura/alugames/data/Database.kt
+++ src/main/kotlin/br/com/alura/alugames/data/Database.kt
@@ -7,14 +7,7 @@ import javax.persistence.Persistence
 
 
 object Database {
-    //    fun getConnection(): Connection? {
-//        return try {
-//            DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm87285", "260402")
-//        } catch (e: SQLException) {
-//            e.printStackTrace()
-//            null
-//        }
-//    }
+
     fun getEntityManager(): EntityManager {
         val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("RM87285")
         return factory.createEntityManager()
